ba39447ce06deac7a0feaad346cd8abd73b69055
mohnndZaghloul <mohanndzaghloul17@gmail.com>
2026-06-28 21:00:26 +0300

    fix responsive

--- components/CardJob.tsx
+++ components/CardJob.tsx
@@ -7,18 +7,20 @@ const CardJob = ({ job }: { job: Job_TP }) => {
       href={job.slug}
       className="rounded-xl border flex justify-between items-center p-5 bg-zinc-900 hover:bg-zinc-800 hover:-translate-1 hover:border-emerald-500 transition duration-200">
       <div>
-        <h3 className="md:text-2xl font-semibold">{job.title}</h3>
+        <h3 className="md:text-2xl font-semibold max-w-full line-clamp-1">
+          {job.title}
+        </h3>
         <span className="text-xs md:text-sm px-2 text-zinc-500">
           {job.company}
         </span>
       </div>
       <div className="flex items-center gap-4">
         <div>
-          <p className="bg-emerald-600/40 border text-xs md:text-sm border-emerald-300 px-4 py-1 rounded-full">
+          <p className="bg-emerald-600/40 text-nowrap border text-[0.75rem] md:text-sm border-emerald-300 px-4 py-1 rounded-full">
             {job.employmentType}
           </p>
         </div>
-        <div className="font-semibold md:text-xl">
+        <div className="font-semibold text-nowrap text-sm md:text-xl">
           {job.salary.currency} {job.salary.value}
         </div>
       </div>
